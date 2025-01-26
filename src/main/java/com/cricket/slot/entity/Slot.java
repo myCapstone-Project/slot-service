7880a9c2f66ba011bc1b64b99dfc94863925541f
vishrutth5214 <vishrutth5214@gmail.com>
2025-01-26 18:31:16 +0530

    slot-service

---
+++ src/main/java/com/cricket/slot/entity/Slot.java
@@ -0,0 +1,26 @@
+package com.cricket.slot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "slot_details")
+public class Slot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+}
